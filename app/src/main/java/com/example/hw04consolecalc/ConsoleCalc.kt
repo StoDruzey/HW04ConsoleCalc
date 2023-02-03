bcfc4c7f7ff4d4a842e7fb2a9cb46be0aec495d9
StoDruzey <sergelabada@gmail.com>
2023-02-03 01:09:14 +0100

    implementing 1 of unary minus

--- app/src/main/java/com/example/hw04consolecalc/ConsoleCalc.kt
+++ app/src/main/java/com/example/hw04consolecalc/ConsoleCalc.kt
@@ -15,42 +15,45 @@ class ConsoleCalc {
             var expression: String? = readlnOrNull()
             expression = expression ?: ""
             println(expression)
-            expression = expression.replace("""\s+""".toRegex(), "")
-            expression = expression.replace("""[^0123456789,.+-/*()]""".toRegex(), "")
+            expression = expression.replace("""\s+""".toRegex(), "") //deleting all spaces
+            expression = expression.replace("""[^0123456789.+-/*()]""".toRegex(), "") //deleting unsuitable symbols
+            expression = expression.replace("""(?<!(\d+(\.\d+)?)|\))\-""".toRegex(), "~") //converting unary minus to ~
 
             println(expression)
             return expression
         }
 
-        fun convertToRPN(expression: String) {
+        fun convertToRPN(expression: String): List<String> { //converts string to List of Reverse Polish Notation
 
             val rpn = mutableListOf<String>() //Reverse Polish Notation
             val stack = mutableListOf<String>()
             var flagDigit = false
+            var unaryMinusFlag = false
             var tempStore = ""
 
-            fun numberToRPN() { //часто повторяющийся блок 1
-                if (flagDigit) {
+            fun numberToRPN() { //frequently repeated block 1 - push number to rpn from temporary store
+                if (flagDigit && unaryMinusFlag) {
                     rpn.add(tempStore)
                     tempStore = ""
                     flagDigit = false
                 }
             }
 
-            fun pushLastOperationFromStackToRPN () { //часто повторяющийся блок 2
+            fun pushLastOperationFromStackToRPN () { //frequently repeated block 2 - push last operation to rpn from stack
                 rpn.add(stack.last())
                 stack.removeLast()
             }
 
             expression.forEach {
                 when (it) {
+                    '~' -> unaryMinusFlag = true
                     in "+-/*" -> {
                         numberToRPN()
                         //ИЛИ операция на вершине стека приоритетнее, или такого же уровня приоритета как o1
                         //… выталкиваем верхний элемент стека в выходную строку; помещаем операцию o1 в стек
-                        if (stack.isEmpty()) {
+                        if (stack.isEmpty()) { //if stack is empty don't check the ops priopity
                             stack.add(it.toString())
-                        } else {
+                        } else { //checking operation priority
                             when (stack.last()) {
                                 "(" -> stack.add(it.toString())
                                 in "*/" -> {
@@ -77,24 +80,28 @@ class ConsoleCalc {
                             stack.removeLast() //removing "(" from stack
                         }
                     }
-                    in "0123456789.," -> {
+                    in "0123456789." -> {
+                        if (unaryMinusFlag) {
+                            tempStore += "-"
+                            unaryMinusFlag = false
+                        }
                         tempStore += it
                         flagDigit = true
                     }
                 }
-//                stack.forEach { print("$it ") }
-//                println()
-//                rpn.forEach { print("$it ") }
-//                println()
             }
             numberToRPN() //adding number to output string if last
 
-            while (stack.isNotEmpty()) { //Когда входная строка закончилась, выталкиваем все символы из стека в выходную строку
+            while (stack.isNotEmpty()) { //when input string is over, push all the operations from stack to output string
                 pushLastOperationFromStackToRPN()
             }
-            stack.forEach { print("$it ") }
-            println()
-            rpn.forEach { print("$it ") }
+            return rpn
+        }
+
+        fun computer(rpn: List<String>): String {
+            val result: String = ""
+
+            return result
         }
     }
 }
